88766553ade835047551d799245483fed68ddda6
Mahyar <Mahyarbinazadeh09@gmail.com>
2025-12-28 20:39:11 +0330

    Fix the methodes of class Stack

--- stack/stack.py
+++ stack/stack.py
@@ -1,15 +1,42 @@
 class Stack:
-	def __init__(self, size):
-		self._stack = [None] * size
-		self.size = size
-		self._top = 0
+    def __init__(self, size):
+        self.size = size
+        self._top = -1
+        self._stack = [None] * size
 
-	def push(self, data): pass
-	def pop(self): pass
-	def popall(self): pass
+    def isfull(self):
+        if self._top == self.size - 1:
+            return True
+        return False
 
-	def top(self):
-        return self._stack[self._top - 1]
+    def isempty(self):
+        if self._top < 0:
+            return True
+        return False
 
-	def isfull(self):pass
-	def isempty(self): pass
+    def push(self, data):
+        if self.isfull():
+            return False
+        self._top += 1
+        self._stack[self._top] = data
+        return True
+
+    def pop(self):
+        if self.isempty():
+            return False
+        poped = self._stack[self._top]
+        self._top -= 1
+        return poped
+
+    def popall(self):
+        if self.isempty():
+            return False
+        all_stack = self._stack
+        self._top = -1
+        self._stack = [None] * self.size
+        return all_stack
+
+    def top(self):
+        if self.isempty():
+            return None
+        return self._stack[self._top]
